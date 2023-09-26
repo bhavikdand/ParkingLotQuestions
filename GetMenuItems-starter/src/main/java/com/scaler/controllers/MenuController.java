dc3f2a9cd7db7b99799c166c47c52dbcc0716978
bhavikdand <bhavik.dand_1@scaler.com>
2023-09-27 02:37:11 +0530

    Added get menu items question

---
+++ GetMenuItems-starter/src/main/java/com/scaler/controllers/MenuController.java
@@ -0,0 +1,21 @@
+package com.scaler.controllers;
+
+import com.scaler.dtos.*;
+import com.scaler.models.MenuItem;
+import com.scaler.services.MenuService;
+
+import java.util.List;
+
+public class MenuController {
+
+    private MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+
+    public GetMenuItemsResponseDto getMenuItems(GetMenuItemsRequestDto requestDto){
+        return null;
+    }
+}
